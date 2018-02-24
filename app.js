22ac2e962b0a5bdb945a4e34d7d72f1965a9d082
DanielHenry <daniel.henry.39@gmail.com>
2018-02-24 07:43:36 +0700

    Add httpGet() to get json from api

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("just for test");
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+var jsonResult = httpGet('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid');
+alert(jsonResult);
