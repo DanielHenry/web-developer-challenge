1be1523a823a67391baf7aec207a91fff371e158
DanielHenry <daniel.henry.39@gmail.com>
2018-02-25 05:08:53 +0700

    Convert response from json to object and insert them into div

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ function httpGet(theUrl)
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false );
     xmlHttp.send( null );
-    return xmlHttp.responseText;
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+        return xmlHttp.responseText;
+    } else {
+        return '{"isSuccess":"failed","description":"cannot fetch the response"}';
+    }
 }
 
 var jsonResult = httpGet('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid');
-alert(jsonResult);
+var objResult = JSON.parse(jsonResult);
+document.getElementById('content').innerHTML = objResult;
