d218acadeda0eabc285e764d551e648fd2a1901c
DanielHenry <daniel.henry.39@gmail.com>
2018-02-28 05:56:00 +0700

    Process json raw data and insert it into div tag

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ function httpGet(theUrl)
     }
 }
 
-var jsonResult = httpGet('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid');
-//var objResult = JSON.parse(jsonResult);
-document.getElementById('content').innerHTML = jsonResult;
+var jsonResponse = httpGet('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid');
+var objResponse = JSON.parse(jsonResponse);
+//document.getElementById('content').innerHTML = jsonResult;
+var rssArray = objResponse.items;
+var parentDivTag = document.getElementById('content');
+for (var i=0; i<rssArray.length; i++){
+    var childDivTag = document.createElement('div');
+    childDivTag.appendChild(rssArray[i]);
+    parentDivTag.appendChild(childDivTag);
+}
