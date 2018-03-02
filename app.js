35888e04812ecf90f8d434500be383a8afe9964c
DanielHenry <daniel.henry.39@gmail.com>
2018-03-03 05:29:54 +0700

    Get title from api response

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ function httpGet(theUrl)
 var jsonResponse = httpGet('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fwwwid');
 var objResponse = JSON.parse(jsonResponse);
 //document.getElementById('content').innerHTML = jsonResult;
+var titleTags = document.getElementsByTagName('title');
+titleTags[0].innerHTML = objResponse.feed.title;
+
 var rssArray = objResponse.items;
 var parentDivTag = document.getElementById('content');
 for (var i=0; i<rssArray.length; i++){
